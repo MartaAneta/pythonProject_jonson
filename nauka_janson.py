4a04b7ca0812bf18f0f7dc45ab81551d31b780ab
MartaAneta <martaostrowska114@gmail.com>
2023-05-29 11:20:19 +0200

    step2

--- nauka_janson.py
+++ nauka_janson.py
@@ -2,10 +2,36 @@ import json
 
 
 # some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
+x1 =  '{ "name":"John", "age":30, "city":"New York"}'
 
 # parse x:
-y = json.loads(x)
+y1 = json.loads(x1)
 
 # the result is a Python dictionary:
-print(y["age"])
+print(y1["age"])
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+x2 = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+print(json.dumps(x2, indent=4, separators=(". ", " = ")))
